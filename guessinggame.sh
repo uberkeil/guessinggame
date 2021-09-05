8bff469d6b05bef7ba8a16d2afec0273661137b4
uberkeil <kyle.p.simpson@gmail.com>
2021-09-05 17:06:05 +1200

    Finalized version of guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,35 +1,3 @@
-# The guessinggame.sh program should have the following behavior:
-# 
-# When the program starts the user should be asked how many files are in the current directory, and then the user should be prompted for a guess.
-# If the user's answer is incorrect the user should be advised that their guess was either too low or too high and then they should be prompted to try to guess again.
-# If the user's guess is correct then they should be congratulated and the program should end.
-# The program should not end until the user has entered the correct number of files in the current directory.
-# The program should be able to be run by entering bash guessinggame.sh into the console.
-# The program should contain at least one function, one loop, and one if statement.
-# The program should be more than 20 lines of code but less than 50 lines of code.
-# The guessinggame.sh program should have the following behavior:
-# 
-# When the program starts the user should be asked how many files are in the current directory, and then the user should be prompted for a guess.
-
-# If the user's answer is incorrect the user should be advised that their guess was either too low or too high and then they should be prompted to try to guess again.
-
-# If the user's guess is correct then they should be congratulated and the program should end.
-
-# The program should not end until the user has entered the correct number of files in the current directory.
-
-# The program should be able to be run by entering bash guessinggame.sh into the console.
-
-# The program should contain at least one function, one loop, and one if statement.
-
-# The program should be more than 20 lines of code but less than 50 lines of code.
-
-
-
-
-
-
-# Piecing it all together
-
 #!/bin/sh
 #Guessing game: Guess number of files in current directory 
 
@@ -70,5 +38,6 @@ ask_function () {
 	echo "You guessed $guess files.  Let me think about this...."
 	check_guess
 }
+
 ask_function
 
